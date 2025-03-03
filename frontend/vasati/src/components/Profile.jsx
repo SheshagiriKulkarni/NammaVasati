0fc06d054dca4787ebfb56d802961825ffa647d2
Shashank Kulkarni <kulkarnishashank962@gmail.com>
2025-03-03 09:26:10 +0530

    remains

--- frontend/vasati/src/components/Profile.jsx
+++ frontend/vasati/src/components/Profile.jsx
@@ -13,7 +13,29 @@ function Profile() {
       <Navbar />
       <div className="box-container">
         <div className="main-cont">
-          <div className="left-cont"></div>
+          <div className="left-cont">
+            <div className="image-conta">
+              <div className="image-field"></div>
+            </div>
+            <div className="email-cont">
+              <div className="email">
+                Email: {localStorage.getItem("userEmail") || "No Email Found"}
+              </div>
+            </div>
+
+            <div className="date-cont">
+              <div className="date">Joined Date: 28-02-2025</div>
+            </div>
+
+            <div className="btns">
+              <button>
+                Ads Posted : <span>5</span>
+              </button>
+              <button>
+                Total Veiws : <span>15</span>
+              </button>
+            </div>
+          </div>
           <div className="right-cont"></div>
         </div>
       </div>
