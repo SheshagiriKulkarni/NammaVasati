df78c5023559c2353d12e28348fbe8a697abeeea
Shashank Kulkarni <kulkarnishashank962@gmail.com>
2025-03-11 15:02:31 +0530

    wishlist

--- frontend/vasati/src/components/Wishlist.jsx
+++ frontend/vasati/src/components/Wishlist.jsx
@@ -10,6 +10,7 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import ChatModal from "./ChatModal";
+import LoadingScreen from "./LoadingScreen";
 
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
@@ -98,6 +99,7 @@ function Wishlist() {
   const [isChatOpen, setIsChatOpen] = useState(false);
   const navigate = useNavigate();
   const userEmail = localStorage.getItem("userEmail");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const storedWishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
@@ -118,13 +120,16 @@ function Wishlist() {
       }
 
       try {
+        setLoading(true);
         const response = await axios.post(
           "http://localhost:5000/api/advertise/wishlist",
           { wishlist }
         );
         setPgs(response.data.map((pg) => ({ ...pg, currentImageIndex: 0 })));
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching wishlisted PG details:", error);
+        setLoading(false);
       }
     };
 
@@ -203,152 +208,158 @@ function Wishlist() {
 
   return (
     <div>
-      <Navbar />
-      <div className="myads-container">
-        <div className="filtered-pgs">
-          {pgs.length > 0 ? (
-            <div className="result-box">
-              <div className="content-real">
-                {pgs.map((ad) => (
-                  <div
-                    className="searched-pg"
-                    onMouseEnter={() =>
-                      setMapCenter([ad.latitude, ad.longitude])
-                    }
-                  >
-                    <div key={ad._id} className="image-content">
-                      {ad.images && ad.images.length > 0 ? (
-                        <>
-                          <button
-                            className="nav-button prev-button"
-                            onClick={() => prevImage(ad._id)}
-                          >
-                            &#8249;
-                          </button>
-                          <img
-                            src={`http://localhost:5000/api/advertise/images/${
-                              ad.images[ad.currentImageIndex]
+      {loading ? (
+        <LoadingScreen /> // Show loading screen when loading is true
+      ) : (
+        <>
+          <Navbar />
+          <div className="myads-container">
+            <div className="filtered-pgs">
+              {pgs.length > 0 ? (
+                <div className="result-box">
+                  <div className="content-real">
+                    {pgs.map((ad) => (
+                      <div
+                        className="searched-pg"
+                        onMouseEnter={() =>
+                          setMapCenter([ad.latitude, ad.longitude])
+                        }
+                      >
+                        <div key={ad._id} className="image-content">
+                          {ad.images && ad.images.length > 0 ? (
+                            <>
+                              <button
+                                className="nav-button prev-button"
+                                onClick={() => prevImage(ad._id)}
+                              >
+                                &#8249;
+                              </button>
+                              <img
+                                src={`http://localhost:5000/api/advertise/images/${
+                                  ad.images[ad.currentImageIndex]
+                                }`}
+                                alt={`PG Image ${ad.currentImageIndex + 1}`}
+                                className="ad-image"
+                              />
+                              <button
+                                className="nav-button next-button"
+                                onClick={() => nextImage(ad._id)}
+                              >
+                                &#8250;
+                              </button>
+                            </>
+                          ) : (
+                            <p>No images available</p>
+                          )}
+
+                          <FaHeart
+                            className={`wishlist-icon ${
+                              wishlist.includes(ad._id) ? "active" : ""
                             }`}
-                            alt={`PG Image ${ad.currentImageIndex + 1}`}
-                            className="ad-image"
+                            onClick={(e) => {
+                              e.stopPropagation(); // Prevent triggering card click
+                              toggleWishlist(ad._id);
+                            }}
                           />
-                          <button
-                            className="nav-button next-button"
-                            onClick={() => nextImage(ad._id)}
-                          >
-                            &#8250;
-                          </button>
-                        </>
-                      ) : (
-                        <p>No images available</p>
-                      )}
-
-                      <FaHeart
-                        className={`wishlist-icon ${
-                          wishlist.includes(ad._id) ? "active" : ""
-                        }`}
-                        onClick={(e) => {
-                          e.stopPropagation(); // Prevent triggering card click
-                          toggleWishlist(ad._id);
-                        }}
-                      />
-                    </div>
-                    <div
-                      className="about-ad"
-                      onClick={() => handleAdClick(ad._id)}
-                    >
-                      <div className="rowone">
-                        <div>
-                          <h3>{`${ad.pgName}`}</h3>
-                          <p title={ad.locationName}>
-                            {ad.locationName.length > 15
-                              ? ad.locationName.slice(0, 15) + "..."
-                              : ad.locationName}
-                          </p>
                         </div>
-                        <div>
-                          <GenderTag gender={ad.gender} />
-                        </div>
-                      </div>
-                      <div className="rowtwo">
-                        <p className="occupancy">
-                          <img
-                            class="occupancy-icon"
-                            src="https://res.cloudinary.com/stanza-living/image/upload/v1700809285/Website%20v5/Icons/tabler_bed.png"
-                          />
-                          {ad.occupancy}
-                        </p>
-                      </div>
-
-                      <div className="rowthree">
-                        <div>₹ {ad.price} / month</div>
-                        <button className="vi-btn">Schedule a Visit</button>
-                        <button
-                          className="ca-btn"
-                          onClick={(e) => {
-                            e.stopPropagation(); // Prevent navigation
-                            openChat(ad.mailid);
-                          }}
+                        <div
+                          className="about-ad"
+                          onClick={() => handleAdClick(ad._id)}
                         >
-                          Request a Callback
-                        </button>
+                          <div className="rowone">
+                            <div>
+                              <h3>{`${ad.pgName}`}</h3>
+                              <p title={ad.locationName}>
+                                {ad.locationName.length > 15
+                                  ? ad.locationName.slice(0, 15) + "..."
+                                  : ad.locationName}
+                              </p>
+                            </div>
+                            <div>
+                              <GenderTag gender={ad.gender} />
+                            </div>
+                          </div>
+                          <div className="rowtwo">
+                            <p className="occupancy">
+                              <img
+                                class="occupancy-icon"
+                                src="https://res.cloudinary.com/stanza-living/image/upload/v1700809285/Website%20v5/Icons/tabler_bed.png"
+                              />
+                              {ad.occupancy}
+                            </p>
+                          </div>
+
+                          <div className="rowthree">
+                            <div>₹ {ad.price} / month</div>
+                            <button className="vi-btn">Schedule a Visit</button>
+                            <button
+                              className="ca-btn"
+                              onClick={(e) => {
+                                e.stopPropagation(); // Prevent navigation
+                                openChat(ad.mailid);
+                              }}
+                            >
+                              Request a Callback
+                            </button>
+                          </div>
+                        </div>
                       </div>
-                    </div>
+                    ))}
                   </div>
-                ))}
-              </div>
 
-              <div className="map-container">
-                <MapContainer
-                  center={mapCenter}
-                  zoom={15}
-                  className="leaflet-container"
-                >
-                  <ChangeMapCenter center={mapCenter} />
-                  <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                  {pgs.map((ad) => (
-                    <Marker
-                      key={ad._id}
-                      position={[ad.latitude, ad.longitude]}
-                      icon={customIcon}
+                  <div className="map-container">
+                    <MapContainer
+                      center={mapCenter}
+                      zoom={15}
+                      className="leaflet-container"
                     >
-                      <Popup>
-                        <strong>{ad.pgName}</strong>
-                        <br />
-                        {ad.locationName}
-                        <br />₹ {ad.price} / month
-                      </Popup>
-                    </Marker>
-                  ))}
-                </MapContainer>
-              </div>
+                      <ChangeMapCenter center={mapCenter} />
+                      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                      {pgs.map((ad) => (
+                        <Marker
+                          key={ad._id}
+                          position={[ad.latitude, ad.longitude]}
+                          icon={customIcon}
+                        >
+                          <Popup>
+                            <strong>{ad.pgName}</strong>
+                            <br />
+                            {ad.locationName}
+                            <br />₹ {ad.price} / month
+                          </Popup>
+                        </Marker>
+                      ))}
+                    </MapContainer>
+                  </div>
+                </div>
+              ) : (
+                <>
+                  <div className="no-pgs">
+                    <div className="back-img"></div>
+                    <p className="link">
+                      <span onClick={() => navigate("/AllAddetails")}>
+                        Click Me
+                      </span>{" "}
+                      To Add Ads To Your Wishlist.
+                    </p>
+                  </div>
+                </>
+              )}
             </div>
-          ) : (
-            <>
-              <div className="no-pgs">
-                <div className="back-img"></div>
-                <p className="link">
-                  <span onClick={() => navigate("/AllAddetails")}>
-                    Click Me
-                  </span>{" "}
-                  To Add Ads To Your Wishlist.
-                </p>
-              </div>
-            </>
+          </div>
+
+          {isChatOpen && (
+            <ChatModal
+              roomId={chatRoomId}
+              senderEmail={userEmail}
+              receiverEmail={chatOwner}
+              onClose={closeChat}
+            />
           )}
-        </div>
-      </div>
 
-      {isChatOpen && (
-        <ChatModal
-          roomId={chatRoomId}
-          senderEmail={userEmail}
-          receiverEmail={chatOwner}
-          onClose={closeChat}
-        />
+          <Footer />
+        </>
       )}
-
-      <Footer />
     </div>
   );
 }
